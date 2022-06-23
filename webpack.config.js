a322edcd46497896f9dac1aa1ccbd79ed7740d98
Arun Kumar <arunkumar413@gmail.com>
2022-06-23 15:50:47 +0000

    changing main

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,48 @@
-"use strict";
-
-var path = require("path");
+const path = require('path');
 
 module.exports = {
-  mode: "production",
-  entry: "./src/index.js",
+  mode: 'production',
+  entry: './src/index.js',
   output: {
-    path: path.resolve("lib"),
-    filename: "EasySelect.js",
-    libraryTarget: "commonjs2"
+    path: path.resolve('lib'),
+    filename: 'EasySelect.js',
+    libraryTarget: 'commonjs2',
   },
   module: {
-    rules: [{
-      test: /\.jsx?$/,
-      exclude: /(node_modules)/,
-      use: "babel-loader"
-    }]
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ],
+  },
+  resolve: {
+    alias: {
+      'react': path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+    }
+  },
+  externals: {
+    // Don't bundle react or react-dom      
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "React",
+      root: "React"
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "ReactDOM",
+      root: "ReactDOM"
+    }
   }
 };
